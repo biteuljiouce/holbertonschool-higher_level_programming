00248c9ce4fc4e0cb8cc4b8a1d4c6537415eec75
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-03-28 10:17:05 +0100

    add newline at the end of result

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,7 @@
 for i in range(97, 123):
     if chr(i) == 'q' or chr(i) == 'e':
         continue
-    print("{}".format(chr(i)), end="")
+    endChar = ""
+    if i == 122:
+        endChar = "\n"
+    print("{}".format(chr(i)), end=endChar)
