744311745b47ffb220c3fcafbdaf4a0a7fd456cd
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-03-28 10:46:59 +0100

    try to fix checker error, raising TypeError exception

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if isinstance(number, int):
-    lastDigit = int(str(number)[-1])
-    if number < 0:
-        lastDigit = lastDigit * -1
-    print("Last digit of {:n} is {:n} ".format(number, int(lastDigit)), end="")
-    if lastDigit > 5:
-        print("and is greater than 5")
-    elif lastDigit == 0:
-        print("and is 0")
-    elif lastDigit < 6:
-        print("and is less than 6 and not 0")
+if not type(number) is int:
+    raise TypeError()
+lastDigit = int(str(number)[-1])
+if number < 0:
+    lastDigit = lastDigit * -1
+print("Last digit of {:n} is {:n} ".format(number, int(lastDigit)), end="")
+if lastDigit > 5:
+    print("and is greater than 5")
+elif lastDigit == 0:
+    print("and is 0")
+elif lastDigit < 6:
+    print("and is less than 6 and not 0")
