9957944c84bf36189b69f86c3dce2efeacd69549
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-03-28 14:32:15 +0100

    new ex

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(input):
+    if not type(input) is int:
+        raise TypeError("arg must be a integer")
+    inputAsStr = str(input)
+    lastChar = inputAsStr[len(inputAsStr)-1]
+    print("{}".format(lastChar))
