44962b28c2dc6b2a7a4c2223b774e6a17aad35af
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-03-28 10:33:45 +0100

    remove newline

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,6 +3,6 @@ for i in range(97, 123):
     if chr(i) == 'q' or chr(i) == 'e':
         continue
     endChar = ""
-    if i == 122:
-        endChar = "\n"
+#    if i == 122:
+#        endChar = "\n"
     print("{}".format(chr(i)), end=endChar)
