030954393cd925d1f1bacae27606a7db6b3f863f
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-03-29 17:43:39 +0100

    try to fix ex0. checker feedback still unusable

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# 0-square.py
-# Cedric Houvenagel
 """Define a class Square."""
 
 
 class Square:
-    """Implement a square (geometrical object)"""
+    """Represents a square (geometrical object)"""
     pass
