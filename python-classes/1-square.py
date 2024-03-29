731455d8418f17b01c6ca58655c2786c37ad06e3
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-03-29 15:38:28 +0100

    new ex

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 1-square.py
+# Cedric Houvenagel
+
+"""Define a class Square."""
+
+
+class Square:
+
+    """Represents a square (geometrical object)"""
+
+    def __init__(self, size):
+        self.__size = size
