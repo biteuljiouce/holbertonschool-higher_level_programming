6c2e6988e0635a9c908619ce814aec3db4c85e98
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-02 18:52:35 +0200

    fix ex2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -17,15 +17,20 @@ def matrix_divided(matrix, div):
         if len(line) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
     # Check if all value are int or float
-    # if ok, calculate result
-    res_matrix = []
-    res_line = []
     for line in matrix:
         for val in line:
             if not isinstance(val, int) and not isinstance(val, float):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            else:
-                res_line.append(round(val / div, 2))
+    if not isinstance(div, int):
+        raise TypeError("div must be an integer")
+    # calculate result
+    res_matrix = []
+    for line in matrix:
+        res_line = []
+        for val in line:
+            res_line.append(round(val / div, 2))
         else:
             res_matrix.append(res_line)
     return res_matrix
+
+print(matrix_divided( [[3, "9"], [15, 3]], 2))
