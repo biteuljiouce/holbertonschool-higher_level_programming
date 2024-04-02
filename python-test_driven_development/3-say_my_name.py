714704033305c32c8be961f045d997b722a69d82
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-02 16:48:36 +0200

    add ex3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Module for Say my name function.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints My name is <first name> <last name>.
+    """
+    # Check if each arg is a string
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not last_name(val, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {}} {}}".format(first_name, last_name))
