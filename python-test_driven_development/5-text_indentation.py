04b8cffd892851c39414cf36bd46f221c933fb43
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-03 13:20:14 +0200

    ex5 : fix style

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,6 @@
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
-    There should be no space at the beginning or at the end of each printed line
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
