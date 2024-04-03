dc954389e394250141686386241368907ca9c9f2
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-03 13:43:43 +0200

    ex5 : add tests in unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,3 +6,13 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 5, 3]), 5)
     def test_empty(self):
         self.assertEqual(max_integer(), None)
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([1, 2, 5]), 5)
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([6, 1, 2, 5]), 6)
+    def test_with_one_neg(self):
+        self.assertEqual(max_integer([6, -1, 2, 5]), 6)
+    def test_with_only_neg(self):
+        self.assertEqual(max_integer([-6, -1, -2, -5]), -1)
+    def test_only_one(self):
+        self.assertEqual(max_integer([666]), 666)
