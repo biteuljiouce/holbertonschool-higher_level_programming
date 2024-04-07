2a0583d567cd4765aa7a3636189a1860b2d9d9c4
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-07 15:33:24 +0200

    ex0 : try to fix style

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
 
-/*
-// print process.argv
-process.argv.forEach(function (val, index, array) {
-	console.log(index + ': ' + val);
-});
-*/
-
 // Include fs module
 fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
-  if (err) {
-	console.log(err);
-  	}
-	else
-  console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else
+        console.log(data);
 });
