482f817c0f8a85ec2e012571c81ab2d5d466b47e
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-07 15:19:57 +0200

    ex0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin/node
+
+/*
+// print process.argv
+process.argv.forEach(function (val, index, array) {
+	console.log(index + ': ' + val);
+});
+*/
+
+// Include fs module
+var fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function(err, data) {
+  if (err) {
+	console.log(err);
+  	}
+	else
+  console.log(data);
+});
